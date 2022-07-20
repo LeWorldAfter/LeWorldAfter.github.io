4c4aa688787275c2c65b74e2154a0f2f0a337cfa
EltoCode <ef356@live.mdx.ac.uk>
2022-07-20 20:13:11 +0400

    made app.js use particles.json

--- templates/resource/particles-js/app.js
+++ templates/resource/particles-js/app.js
@@ -3,14 +3,14 @@
 /* ----------------------------------------------- */
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
+
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('particles.js loaded - callback');
 });
-*/
+
 
 /* Otherwise just put the config content (json): */
-
+/*
 particlesJS('particles-js',
   
   {
@@ -130,4 +130,4 @@ particlesJS('particles-js',
     }
   }
 
-);
+);*/
