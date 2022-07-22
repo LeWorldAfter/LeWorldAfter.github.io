7428169de79e3f92efcad33c047473ac60783116
jerinjoseph121 <jerinjosephch@gmail.com>
2022-07-22 20:13:23 +0530

    Added Image Carousel but I think I don't need it

--- content/players/mr-patcher.md
+++ content/players/mr-patcher.md
@@ -6,6 +6,10 @@ date = 2019-11-27
 name = "Mr_Patcher"
 profile_pic = "img/catwhat.webp"
 
+bg_img_1 = "img/player-img/steve-img/background-img/Steve_Cover_Pic-1.jpg"
+bg_img_2 = "img/player-img/steve-img/background-img/Steve_Cover_Pic-2.jpg"
+bg_img_3 = "img/player-img/steve-img/background-img/Steve_Cover_Pic-3.jpg"
+
 +++
 
 Mr_Patcher is the 6th member of LWA. He joined on XX/XX/2018. His base is at X,X
---
+++ static/js/player.js
@@ -0,0 +1,15 @@
+// document.body.innerHTML = "This is some content";
+
+// function setBackgroundImg(carouselItemElement){
+//     const carouselImg = carouselItemElement.dataset.img;
+
+//     console.log(carouselImg);
+
+//     carouselItemElement.style.backgroundImage = carouselImg;
+// }
+
+// var carouselItemElements = document.getElementById("backgroundCarousel");
+
+// console.log(carouselItemElements);
+
+// carouselItemElements.forEach(setBackgroundImg);
